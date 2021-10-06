0c1618ec6538ff51b1e039117fe213148f129f12
Natxo91 <iglarra91@gmail.com>
2021-10-06 20:40:24 +0200

    unit_test

--- get-function/test_handler.py
+++ get-function/test_handler.py
@@ -2,7 +2,7 @@ import json
 
 import pytest
 
-from app import getfunction
+import getfunction
 
 @pytest.fixture()
 def apigw_event():
--- put-function/test_handler.py
+++ put-function/test_handler.py
@@ -2,7 +2,7 @@ import json
 
 import pytest
 
-from app import putfunction
+import putfunction
 
 
 
