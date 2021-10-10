81cc3718ffe2ad1a45cb96f3be23dbf4ebee5d0f
Natxo91 <iglarra91@gmail.com>
2021-10-10 19:34:15 +0200

    getfunction-unit-test

--- get-function/test_handler.py
+++ get-function/test_handler.py
@@ -69,11 +69,7 @@ def test_lambda_handler(apigw_event,context):
 
     ret = app.getfunction(apigw_event, "")
     data = json.loads(ret["body"])
-    #if 1 == 0:
-    #    ret = 'fatal error 404'
-    #else:
-    #    ret = 'OK'
-    #assert ret == 'OK'
+    
     assert ret["statusCode"] == 200
     #assert "message" in ret["body"]
     #assert data["message"] == "hello world"
